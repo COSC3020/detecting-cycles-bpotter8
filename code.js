a3848d1ab9240fc39fa2dcf2ee5c46abc92c5669
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-05-03 20:53:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function hasCycle(graph) {
+    var visited = [];
+    var current = [];
+
+    function cycle(node) {
+        if (!visited.includes(node)) {
+            visited.push(node);
+            current.push(node);
+
+            for (var edgeNode of graph[node]) {
+                if (!visited.includes(edgeNode)) {
+                    if (cycle(edgeNode)) {
+                        return true;
+                    }
+                    
+                } else if (current.includes(edgeNode)) {
+                    return true;
+                }
+            }
+        }
+        
+        current.pop(node);
+        return false;
+    }
+
+    for (var node in graph) {
+        if (cycle(node)) {
+            return true;
+        }
+    }
+
     return false;
 }
