21dee4e2d79c0933a0e5d6fff9550951993aab6f
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-05-03 21:01:31 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -11,5 +11,34 @@ const graph1 = {
     3: [3]
 };
 
+const graph2 = {
+    0: [1, 2],
+    1: [2],
+    2: [3],
+    3: []
+};
+
+const graph3 = {
+    0: [1, 2, 3],
+    1: [2],
+    2: [0, 1]
+};
+
+const graph4 = {
+    0: [1, 2],
+    1: [3],
+    2: [1],
+    3: [4],
+    4: []
+};
+
+const graph5 = {
+    0: []
+};
+
 // Unit testing
 assert.deepStrictEqual(hasCycle(graph1), true); 
+assert.deepStrictEqual(hasCycle(graph2), false); 
+assert.deepStrictEqual(hasCycle(graph3), true); 
+assert.deepStrictEqual(hasCycle(graph4), false); 
+assert.deepStrictEqual(hasCycle(graph5), false); 
